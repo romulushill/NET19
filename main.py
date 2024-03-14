303cd6538b586dae8f258d80e5124e0b70f71ed3
romulushill <romulushill@robottik.co.uk>
2024-03-14 18:46:06 +0000

    Removed unnecessary Ethnicity parameter.
    Preparing to develop a more complex network consisting of multiple networks.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 # Define the neural network architecture
 
 class Main:
-    def __init__(self, aspects_reference=["darkness", "damaged", "speeding", "gender", "size", "ethnicity"]):
+    def __init__(self, aspects_reference=["darkness", "damaged", "speeding", "gender", "size"]):
         self.aspects_reference = aspects_reference
         self.trainer = Training(
             database_reference="./resources/datastore.json", aspects_reference=self.aspects_reference, result_reference = "criminal"
@@ -28,7 +28,6 @@ class Main:
                 car_properties["speeding"],
                 car_properties["gender"],
                 car_properties["size"],
-                car_properties["ethnicity"]
             ]
         )
         # Reshaping Procedure for the array to fit the model
@@ -61,16 +60,14 @@ while True:
                 speeding_input = input("Speeding --> ")
                 gender_input = input("Gender --> ")
                 size_input = input("Size --> ")
-                ethnicity_input = input("Ethnicity --> ")
             else:
                 darkness_input = random.uniform(0,1)
                 damaged_input = random.uniform(0, 1)
                 speeding_input = random.uniform(0, 1)
                 gender_input = random.uniform(0, 1)
                 size_input = random.uniform(0, 1)
-                ethnicity_input = random.uniform(0,1)
 
-            test_dict = {"darkness": float(darkness_input), "damaged": float(damaged_input), "speeding": float(speeding_input), "gender":float(gender_input),"size":float(size_input),"ethnicity":float(ethnicity_input)}
+            test_dict = {"darkness": float(darkness_input), "damaged": float(damaged_input), "speeding": float(speeding_input), "gender":float(gender_input),"size":float(size_input)}
             print(test_dict)
             is_criminal = network.predict_criminal_car(test_dict)
             print(f"Criminal Vehicle: {is_criminal}")
